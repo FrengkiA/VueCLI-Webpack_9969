0da5b0cce1708a5b28050767e39f93c641c24e72
FrengkiA <frengki0611@gmail.com>
2020-11-15 14:57:20 +0700

    Tugas Vue Basic

--- vuecli-webpack/src/router.js
+++ vuecli-webpack/src/router.js
@@ -34,7 +34,13 @@ const router = new VueRouter({
                 path: "/ugd",
                 name: "Unguided",
                 component: importComponent('TodoList/ListUGD'),
-            },
+                },
+
+                {
+                    path: "/tgs",
+                    name: "Tugas",
+                    component: importComponent('TodoList/ListTGS'),
+                },
            ]
        },
    ]
